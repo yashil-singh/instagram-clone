906c39fa439a27405cf31dbccded7787c4d81f4b
Yashil Lal Singh <yashilsingh0208@gmail.com>
2024-07-05 21:14:48 +0545

    Added project files with initial setup

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { Tabs } from "expo-router";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{ tabBarActiveTintColor: "black", tabBarShowLabel: false }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerTitle: "For You",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="new"
+        options={{
+          headerTitle: "Create Post",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="plus-square-o" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          headerTitle: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" size={26} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/(tabs)/new.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function New() {
+  return <Text>Feed</Text>;
+}
